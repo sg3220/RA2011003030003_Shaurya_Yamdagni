f7f1d016fc6661a386ad7b4e3b7590a748733e04
Siddharth Ghosh <email>
2023-08-22 21:07:13 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider, CSSReset, Container } from "@chakra-ui/react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Header from "./Header";
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import SignupPage from "./SignupPage";
+import DetailedProduct from "./DetailedProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <CSSReset />
+      <Router>
+        <Header />
+        <Container maxW="1200px" mt="8">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/DetailedProduct/:id" element={<DetailedProduct />} />
+          </Routes>
+        </Container>
+      </Router>
+    </ChakraProvider>
   );
 }
 
